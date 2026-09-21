bc5e22d4f0718a5d05074b3666ae89d49d4b5710
Saish <saishnotfound@gmail.com>
2026-09-21 18:31:57 +0530

    fix: Import TabRowDefaults.tabIndicatorOffset in PathsScreen.kt

--- Manager/app/src/main/java/com/voidkernel/voidfs/ui/screens/paths/PathsScreen.kt
+++ Manager/app/src/main/java/com/voidkernel/voidfs/ui/screens/paths/PathsScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.FolderOff
 import androidx.compose.material3.*
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
